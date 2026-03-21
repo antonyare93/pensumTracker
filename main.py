524e94ab1155a5bf38373772cded76b1c82a856c
Antony Arevalo <16087644+antonyare93@users.noreply.github.com>
2026-03-20 19:14:09 -0500

    Render files

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ from routers.academic_router import router
 
 app = FastAPI(title="Pensum Tracker API")
 
+import os
+
+_extra = os.getenv("ALLOWED_ORIGINS", "")
+_origins = [o.strip() for o in _extra.split(",") if o.strip()]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        *_origins,
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
